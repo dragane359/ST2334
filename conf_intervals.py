af95557e585163ab4467d8262b5318a97e9607e3
Ganesh <e0552199@u.nus.edu>
2022-04-23 00:18:57 +0800

    conf interval codes added

--- conf_intervals.py
+++ conf_intervals.py
@@ -1,8 +1,64 @@
 import scipy.stats as stats
 from math import *
 
-def known_mean_variance_2_sided(mean, alpha, n, variance):
-    left = mean + stats.norm.ppf(alpha)*(variance/sqrt(n))
-    right = mean + stats.norm.ppf(1 - alpha)*(variance/sqrt(n))
+def known_mean_variance(mean, alpha, n, sd): # alpha/2
+    left = mean + stats.norm.ppf(alpha)*(sd/sqrt(n))
+    right = mean + stats.norm.ppf(1 - alpha)*(sd/sqrt(n))
     return (left, right)
 
+def unknown_mean_variance_lessthan30(mean, alpha, df, n, samplesd): #df -1 first, alpha/2
+    left = mean + stats.t(df).ppf(alpha)*(samplesd/sqrt(n))
+    right = mean + stats.t(df).ppf(1 - alpha)*(samplesd/sqrt(n))
+    return (left, right)
+
+def unknown_mean_variance_morethan30(mean, alpha, n, samplesd): #df -1 first, alpha/2
+    left = mean + stats.norm.ppf(alpha)*(samplesd/sqrt(n))
+    right = mean + stats.norm.ppf(1 - alpha)*(samplesd/sqrt(n))
+    return (left, right)
+
+def diffbtw_known_mean_variance(mean1, mean2, alpha, n1, n2, sd1, sd2): #df -1 first, alpha/2
+    left = mean1 - mean2 + stats.norm.ppf(alpha)*(sqrt(((sd1**2)/n1) + ((sd2**2)/n2)))
+    right = mean1 - mean2 + stats.norm.ppf(1 - alpha)*(sqrt(((sd1**2)/n1) + ((sd2**2)/n2)))
+    return (left, right)
+
+def diffbtw_unknown_mean_variance_morethan30(mean1, mean2, alpha, n1, n2, sampsd1, sampsd2): #df -1 first, alpha/2
+    left = mean1 - mean2 + stats.norm.ppf(alpha)*(sqrt(((sampsd1**2)/n1) + ((sampsd2**2)/n2)))
+    right = mean1 - mean2 + stats.norm.ppf(1 - alpha)*(sqrt(((sampsd1**2)/n1) + ((sampsd2**2)/n2)))
+    return (left, right)
+
+def diffbtw_unknown_butequal_variance_lessthan30(mean1, mean2, alpha, n1, n2, sampsd1, sampsd2): #df -1 first, alpha/2
+    sqp = ((n1 - 1) * sampsd1**2 + (n2 - 1) * sampsd2**2) / (n1 + n2 - 2)
+    left = mean1 - mean2 + stats.t(n1 + n2 - 2).ppf(alpha) * sqrt(sqp) * sqrt(1/n1 + 1/n2)
+    right = mean1 - mean2 + stats.t(n1 + n2 - 2).ppf(1 - alpha) * sqrt(sqp) * sqrt(1/n1 + 1/n2)
+    return (left, right)
+
+def diffbtw_unknown_butequal_variance_morethan30(mean1, mean2, alpha, n1, n2, sampsd1, sampsd2): #df -1 first, alpha/2
+    sqp = ((n1 - 1) * sampsd1**2 + (n2 - 1) * sampsd2**2) / (n1 + n2 - 2)
+    left = mean1 - mean2 + stats.norm.ppf(alpha) * sqrt(sqp) * sqrt(1/n1 + 1/n2)
+    right = mean1 - mean2 + stats.norm.ppf(1 - alpha) * sqrt(sqp) * sqrt(1/n1 + 1/n2)
+    return (left, right)
+
+def diffbtw_unknown_paired_lessthan30(dbar, n, alpha, sampsd): #df -1 first, alpha/2
+    left = dbar + stats.t(n - 1).ppf(alpha)*(sampsd / sqrt(n))
+    right = dbar + stats.t(n - 1).ppf(1 - alpha)*(sampsd / sqrt(n))
+    return (left, right)
+
+def diffbtw_unknown_paired_morethan30(dbar, n, alpha, sampsd): #df -1 first, alpha/2
+    left = dbar + stats.norm.ppf(alpha)*(sampsd / sqrt(n))
+    right = dbar + stats.norm.ppf(1 - alpha)*(sampsd / sqrt(n))
+    return (left, right)
+
+def confint_sdsquared_mean_unknown(n, alpha, sampsd): #df -1 first, alpha/2
+    left = (n -1)*sampsd**2 / (stats.chi2(n - 1).ppf(1 - alpha))
+    right = (n -1)*sampsd**2 / (stats.chi2(n - 1).ppf(alpha))
+    return (left, right)
+
+def confint_sdsquared_mean_known(n, alpha, sampsd): #df -1 first, alpha/2
+    left = sampsd / (stats.chi2(n).ppf(1 - alpha))
+    right = sampsd / (stats.chi2(n).ppf(alpha))
+    return (left, right)
+
+def confint_ratioof2sd_mean_known(alpha, n1, n2, sampsd1, sampsd2): #df -1 first, alpha/2, take not is sd not sd**2
+    left = sqrt((sampsd1**2/sampsd2**2) * (1/(stats.f(n1 - 1, n2 - 1).ppf(1 - alpha))))
+    right = sqrt((sampsd1**2/sampsd2**2) * ((stats.f(n2 - 1, n1 - 1).ppf(1 - alpha))))
+    return (left, right)
--- tests.py
+++ tests.py
@@ -1,4 +1,15 @@
 from conf_intervals import *
 
-print(known_mean_variance_2_sided(3.5, 0.025, 36, .3))
-print(known_mean_variance_2_sided(3.5, 0.025, 36, .6))
+print(known_mean_variance(3.5, 0.025, 36, .3))
+print(unknown_mean_variance_lessthan30(10, 0.025, 6, 7, sqrt(0.08)))
+print(unknown_mean_variance_morethan30(62.56, 0.05, 50, 20))
+print(diffbtw_known_mean_variance(42, 36, 0.02, 50, 75, 6, 8))
+print(diffbtw_unknown_mean_variance_morethan30(78.3, 87.2, 0.025, 50, 50, 5.6, 6.3))
+print(diffbtw_unknown_butequal_variance_lessthan30(85, 81, 0.05, 12, 10, 4, 5))
+print(diffbtw_unknown_butequal_variance_morethan30(85, 81, 0.05, 12, 10, 4, 5))
+print(diffbtw_unknown_paired_lessthan30(-1.6, 10, 0.01, sqrt(40.71)))
+print(diffbtw_unknown_paired_morethan30(-1.6, 10, 0.01, sqrt(40.71)))
+print(confint_sdsquared_mean_unknown(10, 0.025, sqrt(0.286)))
+print(confint_sdsquared_mean_known(10, 0.025, sqrt(0.286)))
+print(confint_ratioof2sd_mean_known(0.01, 25, 16, 8, 7))
+
